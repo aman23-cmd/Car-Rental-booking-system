d39b562ad1513af75958b3fd41d96d3e73e806c1
aman23-cmd <amankau2000@gmail.com>
2024-12-03 22:43:06 +0530

    Add files via upload

---
+++ Frontend/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Page loaded successfully!");
+});
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Page loaded successfully!");
+});
